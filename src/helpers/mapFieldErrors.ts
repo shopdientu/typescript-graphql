0d8646593f93f0e813b445a3b86ecb08c9c0be2c
shopdientu <shopdientu.tk>
2022-03-16 20:05:17 +0700

    final

--- src/helpers/mapFieldErrors.ts
+++
@@ -1,9 +0,0 @@
-import { FieldError } from './../generated/graphql'
-export const mapFieldErrors = (errors: FieldError[]): { [key: string]: any } =>
-  errors.reduce(
-    (acc, error) => ({
-      ...acc,
-      [error.field]: error.message,
-    }),
-    {}
-  )
